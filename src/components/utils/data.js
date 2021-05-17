82afe299a90af49919bc6c180b9883e0e8c2dacc
Martyna Puzio <martyna.puzio@beclose.eu>
2021-05-17 09:10:28 +0200

    Data addded

---
+++ src/components/utils/data.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        name: 'salads',
+        items: [
+            { name: 'cesar', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' },
+            { name: 'z kozim serem', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' }
+        ]
+    },
+    {
+        name: 'main',
+        items: [
+            { name: 'dorsz', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' },
+            { name: 'pstrąg', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' }
+        ]
+    },
+    {
+        name: 'soups',
+        items: [
+            { name: 'chłodnik', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' },
+            { name: 'brownie', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' }
+        ]
+    },
+    {
+        name: 'desserts',
+        items: [
+            { name: 'sermik', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' },
+            { name: 'brownie', link: 'https://www.kwestiasmaku.com/przepis/salatka-cezar-z-kurczakiem-i-szparagami' }
+        ]
+    },
+
+]
+
+export default data;
